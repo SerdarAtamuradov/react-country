5695012066de7265f0452d7f179ebd001191babb
Serdar <serdaratamuradov24@gmail.com>
2021-07-09 20:42:44 +0300

    after installing apollo&graphql

--- src/components/TableView.tsx
+++ src/components/TableView.tsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState } from "react";
+import { ApolloClient, InMemoryCache, gql, useQuery } from "@apollo/client";
 
-const TableView:React.FC = () => {
-  return (
-    <>
-      
-    </>
-  )
-}
+// initialize a GraphQL client
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  uri: "https://countries.trevorblades.com",
+});
+
+const LIST_COUNTRIES = gql`
+  {
+    countries {
+      name
+      code
+    }
+  }
+`;
+
+const TableView: React.FC = () => {
+  // const [country, setCountry] = useState("US");
+  const { data, loading, error } = useQuery(LIST_COUNTRIES, { client });
+
+  if (loading || error) {
+    return <p>{error ? error.message : "Loading..."}</p>;
+  }
+
+  return <></>;
+};
 
 export default TableView;
