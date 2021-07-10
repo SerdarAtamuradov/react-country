b7401f48c9245585d5e11690ccc8130fe9651d28
Serdar <serdaratamuradov24@gmail.com>
2021-07-10 20:29:56 +0300

    after sorting

--- src/components/TableView.tsx
+++ src/components/TableView.tsx
@@ -9,32 +9,12 @@ const client = new ApolloClient({
   uri: "https://countries.trevorblades.com",
 });
 
-interface IPlace {
-  name: string;
-  code: number;
-}
-
-interface ICountriesinfo extends IPlace {
-  languages: Array<IPlace>;
-  continent: IPlace;
-  dataIndex?: string;
-  record?(): string;
-}
-
-interface ICountry extends IPlace {
-  capital: string;
-  currency: string;
-  phone: string;
-}
-
 const LIST_COUNTRIES = gql`
   {
     countries {
-      name
       code
+      name
       capital
-      phone
-      currency
       languages {
         name
         code
@@ -47,9 +27,38 @@ const LIST_COUNTRIES = gql`
   }
 `;
 
+interface ICountry {
+  name: string;
+  code: number;
+  capital: string;
+}
+
+interface ICountriesinfo extends ICountry {
+  languages: Array<ICountry>;
+  continent: ICountry;
+}
+// dataIndex?: string;
+// record?(): string;
+
 const TableView: React.FC = () => {
   const { data, loading, error } = useQuery(LIST_COUNTRIES, { client });
 
+  if (loading || error) {
+    return <p>{error ? error.message : "Loading..."}</p>;
+  }
+
+  const compareByAlph = (a: string, b: string) => {
+    if (a > b) return 1;
+    if (a < b) return -1;
+    return 0;
+  };
+
+  let data_source: Array<ICountriesinfo> = data.countries.map(
+    (item: ICountriesinfo) => ({ ...item, key: item.code })
+  );
+
+  // console.log(data.countries);
+  // console.log(data_source);
   const columns = [
     {
       title: "Code",
@@ -65,29 +74,26 @@ const TableView: React.FC = () => {
       title: "Capital",
       dataIndex: "capital",
       key: "capital",
+      sorter: (a: ICountry, b: ICountry) => compareByAlph(a.capital, b.capital),
     },
     {
       title: "Languages",
-      render: (record: ICountriesinfo) =>
+      render: (record: ICountriesinfo): string =>
         record.languages.map((item) => item.name).join(", "),
       key: "languages[0].code",
     },
     {
       title: "Continent",
-      render: (record: ICountriesinfo) => record.continent.name,
+      render: (record: ICountriesinfo): string => record.continent.name,
       key: "continent.code",
     },
   ];
 
-  if (loading || error) {
-    return <p>{error ? error.message : "Loading..."}</p>;
-  }
-
   return (
     <>
-      <Table<ICountry>
+      <Table<ICountriesinfo>
         columns={columns}
-        dataSource={data.countries}
+        dataSource={data_source}
         className="table"
       />
     </>
